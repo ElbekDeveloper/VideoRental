edff9c1dd2ec8f92af32c484374cfc1b12a9bf71
Elbek <52498074+ElbekDeveloper@users.noreply.github.com>
2020-08-25 12:20:19 +0500

    hello world: first asp.net web app

---
+++ VideoStoreUI/Controllers/PersonController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+using VideoStoreUI.Models;
+
+namespace VideoStoreUI.Controllers
+{
+    public class PersonController : Controller
+    {
+        // GET: Person/Random
+        public ActionResult Random()
+        {
+            var person = new PersonModel() { Name = "Hello World!" };
+            return View(person);
+        }
+    }
+}
---
+++ VideoStoreUI/Models/PersonModel.cs
@@ -0,0 +1,8 @@
+﻿namespace VideoStoreUI.Models
+{
+    public class PersonModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
