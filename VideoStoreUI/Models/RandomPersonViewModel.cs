3bff105c037d88b5bb06080394687b8171983506
Elbek <52498074+ElbekDeveloper@users.noreply.github.com>
2020-08-27 12:21:06 +0500

    Show list of people

---
+++ VideoStoreUI/Models/RandomPersonViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace VideoStoreUI.Models
+{
+    public class RandomPersonViewModel
+    {
+        public PersonModel PersonModel { get; set; }
+        public List<Friend> Friends { get; set; }
+    }
+
+    public class Friend
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
