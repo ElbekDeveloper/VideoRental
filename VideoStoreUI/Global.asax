d3c68f7bd432b78354f0b5224691f7067719d7e0
Elbek <52498074+ElbekDeveloper@users.noreply.github.com>
2020-09-28 15:16:11 +0500

    upload lesson

--- VideoStoreUI/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="VideoStoreUI.MvcApplication" Language="C#" %>
--- VideoStoreUI/Models/PersonModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace VideoStoreUI.Models
-{
-    public class PersonModel
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-    }
-}
